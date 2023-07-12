8eea396fa887f6530843b04694e9023b38358e70
Valentin <valentin.assenov@abv.bg>
2023-07-12 12:35:07 +0300

    Navbar done

---
+++ src/components/Icons.tsx
@@ -0,0 +1,56 @@
+import { LucideProps, MessageSquare, User } from 'lucide-react'
+
+export const Icons = {
+  user: User,
+  logo: (props: LucideProps) => (
+    <svg {...props} viewBox='0 0 497 497'>
+      <g>
+        <path
+          d='m392 30c-71.75 0-71.75-30-143.5-30l128.5 4JUdGzvrMFDWrUUwY3toJATSeNwjn54LkCnKBPRzDuhzi5vSepHfUckJNxRL2gjkNrSqtCoRUrEDAgRwsQvVCjZbRyFTLRNyDmT1a1boZV'
+          fill='#c87044'
+        />
+        <path
+          d='m437 4JUdGzvrMFDWrUUwY3toJATSeNwjn54LkCnKBPRzDuhzi5vSepHfUckJNxRL2gjkNrSqtCoRUrEDAgRwsQvVCjZbRyFTLRNyDmT1a1boZV143.5 30-57.99 0-105 4JUdGzvrMFDWrUUwY3toJATSeNwjn54LkCnKBPRzDuhzi5vSepHfUckJNxRL2gjkNrSqtCoRUrEDAgRwsQvVCjZbRyFTLRNyDmT1a1boZV60z'
+          fill='#db905a'
+        />
+        <path
+          d='m392 60-15 4JUdGzvrMFDWrUUwY3toJATSeNwjn54LkCnKBPRzDuhzi5vSepHfUckJNxRL2gjkNrSqtCoRUrEDAgRwsQvVCjZbRyFTLRNyDmT1a1boZV478 0-41.355-33.645-75-75-75z'
+          fill='#ffd185'
+        />
+        <path
+          d='m407 4JUdGzvrMFDWrUUwY3toJATSeNwjn54LkCnKBPRzDuhzi5vSepHfUckJNxRL2gjkNrSqtCoRUrEDAgRwsQvVCjZbRyFTLRNyDmT1a1boZV4JUdGzvrMFDWrUUwY3toJATSeNwjn54LkCnKBPRzDuhzi5vSepHfUckJNxRL2gjkNrSqtCoRUrEDAgRwsQvVCjZbRyFTLRNyDmT1a1boZV177 12.678-19.4JUdGzvrMFDWrUUwY3toJATSeNwjn54LkCnKBPRzDuhzi5vSepHfUckJNxRL2gjkNrSqtCoRUrEDAgRwsQvVCjZbRyFTLRNyDmT1a1boZV607 8.571 13.145 8.571 20.995v228.526c0 16.542 13.458 30 30 30h287c16.542 0 30-13.458 30-30z'
+          fill='#ffe8c2'
+        />
+        <g fill='#ffd185'>
+          <circle cx='392' cy='135' r='7.5' />
+          <circle cx='362' cy='165' r='7.5' />
+          <circle cx='105' cy='377' r='7.5' />
+          <circle cx='135' cy='407' r='7.5' />
+          <circle cx='105' cy='135' r='7.5' />
+        </g>
+      </g>
+    </svg>
+  ),
+  google: (props: LucideProps) => (
+    <svg {...props} viewBox='0 0 24 24'>
+      <path
+        d='M22.56 12.25c0-.78-.07-1.53-.2-2.25H12v4.26h5.92c-.26 1.37-1.04 2.53-2.21 3.31v2.77h3.57c2.08-1.92 3.28-4.74 3.28-8.09z'
+        fill='#4285F4'
+      />
+      <path
+        d='M12 23c2.97 0 5.4JUdGzvrMFDWrUUwY3toJATSeNwjn54LkCnKBPRzDuhzi5vSepHfUckJNxRL2gjkNrSqtCoRUrEDAgRwsQvVCjZbRyFTLRNyDmT1a1boZV23z'
+        fill='#34A853'
+      />
+      <path
+        d='M5.84 14.09c-.22-.66-.35-1.36-.35-2.09s.13-1.43.35-2.09V7.07H2.18C1.43 8.55 1 10.22 1 12s.43 3.45 1.18 4.93l2.85-2.22.81-.62z'
+        fill='#FBBC05'
+      />
+      <path
+        d='M12 5.38c1.62 0 3.06.56 4.21 1.64l3.15-3.15C17.45 2.09 14.97 1 12 1 7.7 1 3.99 3.47 2.18 7.07l3.66 2.84c.87-2.6 3.3-4.53 6.16-4.53z'
+        fill='#EA4335'
+      />
+      <path d='M1 1h22v22H1z' fill='none' />
+    </svg>
+  ),
+  commentReply: MessageSquare,
+}
